73eeefb6bf9f82331d29aac68930521cbd269999
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-11-28 12:17:28 +0200

    [lab3] update pollard-p during lab

--- labs/lab3/pollard-p.py
+++ labs/lab3/pollard-p.py
@@ -7,7 +7,25 @@ def gcd(a, b):
     return a
 
 
-def pollard_p(n: int, x0: int, f: Callable[[int], int] = lambda x: x ** 2 + 1) -> int:
+def is_prime(n: int):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+# https://moodle.cs.ubbcluj.ro/pluginfile.php/46228/mod_resource/content/1/pkc-c04.pdf#page=12&zoom=150
+def pollard_p(n: int, x0: int, f: Callable[[int], int] = lambda x: x ** 2 + 1):
+    if is_prime(n):
+        print(f"{n} is prime")
+        return
+
     j = 1
     x_sequence = {0: x0}
 
@@ -28,7 +46,8 @@ def pollard_p(n: int, x0: int, f: Callable[[int], int] = lambda x: x ** 2 + 1) -
 
 
 if __name__ == '__main__':
-    n = 9983
+    n = 2
     x0 = 2
     f1 = pollard_p(n, x0, lambda x: x ** 2 + 1)
-    print(f"{n} = {f1} * {n // f1}")
+    if f1 is not None:
+        print(f"{n} = {f1} * {n // f1}")
